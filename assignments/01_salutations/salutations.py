34791ca5d44d181fd1fc460921755a265e08754b
anesharodriguez <77466964+anesharodriguez@users.noreply.github.com>
2021-09-08 10:57:01 -0700

    salutations_homework

---
+++ assignments/01_salutations/salutations.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Author : anesharodriguez <anesharodriguez@localhost>
+Date   : 2021-09-08
+Purpose: Rock the Casbah
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Rock the Casbah',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+    parser.add_argument('-g',
+                        '--greeting',
+                        help='the name to greet',
+                        metavar='str',
+                         default='Howdy')
+
+    parser.add_argument('-n',
+                        '--name',
+                        help='the name to greet',
+                        metavar='str',
+                       default='Stranger')
+
+    parser.add_argument('-e',
+                        '--excited',
+                        help='Print bang',
+                        action='store_true')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    ending = '!' if args.excited else '.'
+    print(args.greeting +', '+ args.name +ending)
+
+
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
